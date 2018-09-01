a9cde9e7d0a5a85785c49081a82e52a213c861f2
cj444 <42742847+cj444@users.noreply.github.com>
2018-09-01 11:54:27 +0530

    Add files via upload

--- templates/home.php
+++ templates/home.php
@@ -1,68 +1,21 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-
 <head>
     <meta charset="utf-8" />
-    <title>Learning OpenTok PHP</title>
+    <title>OpenTok Hello World</title>
+    <script src="https://static.opentok.com/v2/js/opentok.min.js"></script>
+    <script type="text/javascript">
+        var apiKey = '<?php echo $apiKey; ?>';
+        var sessionId = '<?php echo $sessionId; ?>';
+        var token = '<?php echo $token; ?>';
+    </script>
+    <script src="/js/helloworld.js"></script>
 </head>
-
-<style>
-  p, td {
-    font-family: Arial;
-  }
-  td:first-child {
-    font-family: Consolas, Courier, monospace;
-    padding-right: 20px
-  }
-  tr {
-    border: 1px solid black
-  }
-</style>
-
 <body>
-  <p> getting api and session and token with creating default channel named () </p>
-    <p>
-      <table>
-      <tr>
-        <td>ApiKey  <?php echo $apiKey; ?></td>
-        <td>session ID, and token.   <?php echo $sessionId; ?>  <?php echo ' and '.$token; ?></td>
-      </tr>
-      </table>
-    </p>
-    <p>
-      Resources are defined at the following endpoints:
-    <table>
-      <tr>
-        <td>GET /session </td>
-        <td>Return an OpenTok API key, session ID, and token.</td>
-      </tr>
-      <tr>
-        <td>GET /room/:name</td>
-        <td>Return an OpenTok API key, session ID, and token associated with a room name.</td>
-      </tr>
-      <tr>
-        <td>POST /archive/start</td>
-        <td>Start an archive for the specified OpenTok session.</td>
-      </tr>
-      <tr>
-        <td>POST /archive/:archiveId/stop</td>
-        <td>Stop the specified archive.</td>
-      </tr>
-      <tr>
-        <td>GET /archive/:archiveId/view</td>
-        <td>View the specified archive.</td>
-      </tr>
-      <tr>
-        <td>GET /archive/:archiveId</td>
-        <td>Return metadata for the specified archive.</td>
-      </tr>
-      <tr>
-        <td>GET /archive</td>
-        <td>Return a list of archives. <a href="https://tokbox.com/developer/sdks/node/reference/OpenTok.html#listArchives">More Information</td>
-        <td>Pagination is enabled by applying either count or offset parameteres.</td>
-      </tr>
-    </ul>
-  </p>
-</body>
+    <h2>Channel : </h2>
 
+    <div id="publisher"></div>
+
+    <div id="subscribers"></div>
+</body>
 </html>
